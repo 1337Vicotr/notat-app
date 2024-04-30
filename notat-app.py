c505dceec144b9391d9b98b52367f753d0949f5d
1337Vicotr <victorsjessen@gmail.com>
2024-04-30 09:06:36 +0200

    la til at man kan søke i notater etter tittel

--- notat-app.py
+++ notat-app.py
@@ -6,10 +6,33 @@ c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS notater
              (id INTEGER PRIMARY KEY,
               tittel TEXT,
-              innhold TEXT,
+              tekst TEXT,
               tags TEXT)''')
 
-def opprett_notat(tittel, innhold, tags):
-    c.execute('''INSERT INTO notater (tittel, innhold, tags)
-                 VALUES (?, ?, ?)''', (tittel, innhold, tags))
+def lag_notat(tittel, tekst, tags):
+    c.execute('''INSERT INTO notater (tittel, tekst, tags)
+                 VALUES (?, ?, ?)''', (tittel, tekst, tags))
     conn.commit()
+
+def vis_alle_notater():
+    c.execute('''SELECT * FROM notater''')
+    notater = c.fetchall()
+    for note in notater:
+        print("ID:", note[0])
+        print("Tittel:", note[1])
+        print("Tekst:", note[2])
+        print("Tags:", note[3])
+        print("")
+
+def sok_notater(tittel):
+    c.execute('''SELECT * FROM notater WHERE tittel LIKE ?''', ('%' + tittel + '%',))
+    notater = c.fetchall()
+    for note in notater:
+        print("ID:", note[0])
+        print("Tittel:", note[1])
+        print("Tekst:", note[2])
+        print("Tags:", note[3])
+        print("")
+
+# lag_notat("Tittel test", "Tekst test", "testtag, testtag2")
+sok_notater("Tittel test")
